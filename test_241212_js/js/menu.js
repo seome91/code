8d9ece963202d735eb181d83fdc52d208b51f1c0
seome91 <seome1991@gmail.com>
2024-12-17 17:59:14 +0900

    20241217

---
+++ test_241212_js/js/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    /*
+        pc버전에서 메뉴에 (li냐..a)에 마우스를 오버하면 
+        1.headerdp menu_pc 클래스추가
+        2.header.menu_pc .gnb .gnb_wrap ul.depth1 > li에 active 클래스추가
+
+        >> 이벤트 대상 (마우스 오버 대상) header.menu_pc .gnb .gnb_wrap ul.depth1 > li
+    
+    */ 
+   $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        $('header').addClass('menu_pc')
+        $('header .gnb .gnb_wrap ul.depth1 > li').addClass('active')
+        $(this).addClass('active')
+
+        
+    
+    })
+
+})//$(document).ready
