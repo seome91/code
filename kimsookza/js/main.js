b4d79a18b243669dda517f7c974272eb506bc59d
seome91 <seome1991@gmail.com>
2025-02-12 17:41:04 +0900

    .

--- kimsookza/js/main.js
+++ kimsookza/js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 
-    /**************************** 변수 ************************************/
-    let device_status //브라우저가 pc인지 mobile상태인지
-    let window_w //브라우저의 넓이
-    let scrolling //브라우저가 스크롤 된 값
-    let tab_name //find의 클릭한 tab의 이름
 
     /**************************** visual 팝업 (시작) ************************************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
@@ -37,8 +32,56 @@ $(document).ready(function(){
     })
     /**************************** visual 팝업 (종료) ************************************/
 
-    /**************************** pc인지 mobile 구분 (시작)  ***********************************/
- 
+    
+    const plan_swiper = new Swiper('.plan .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+    
+        //effect: "fade", /* fade 효과 */
+    
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.plan .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            /* type: 'fraction',  type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+        
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.plan .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.plan .btn_prev',  
+        },
+    
+    });
+  /**************************list 플러그인팝업***************************************/
+  const gallery_swiper = new Swiper('.gallery .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 5, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 65, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        navigation: {
+            nextEl: '.gallery .swiper-button-next',
+            prevEl: '.list .swiper-button-prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.gallery .swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+        
 
 })//document.ready
