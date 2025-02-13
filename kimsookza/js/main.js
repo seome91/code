a075ea0c9455420c69c2399e6c1f3abc066dc958
seome91 <seome1991@gmail.com>
2025-02-13 11:59:09 +0900

    .

--- kimsookza/js/main.js
+++ kimsookza/js/main.js
@@ -58,15 +58,14 @@ $(document).ready(function(){
     });
   /**************************list 플러그인팝업***************************************/
   const gallery_swiper = new Swiper('.gallery .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 5, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 65, /* 팝업과 팝업 사이 여백 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
             640: {    /* 640px 이상일때 적용 */
-                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 20,
+                spaceBetween: 65,
             },
         },
-        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        // centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         // autoplay: {  /* 팝업 자동 실행 */
         //     delay: 2500,
