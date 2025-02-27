bd54653cb1209800ff3f385de0e5c42823d3f79e
seome91 <seome1991@gmail.com>
2025-02-27 13:14:50 +0900

    .

--- simpol/js/main.js
+++ simpol/js/main.js
@@ -17,8 +17,35 @@ $(document).ready(function(){
     });
         
     /**************************** visual 팝업 (종료) ************************************/
+
+
+
+    /**************************** offer 팝업 (시작) ************************************/
+    const offer_swiper = new Swiper('.offer .swiper', { 
+
+        loop: true,
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.offer .swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            /*type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            
+		},
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.offer .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.offer .btn_prev',  
+	    },
+	
+        
+
+    });
+
+
+
+
+
+    /**************************** offer 팝업 (종료) ************************************/
        
-
-
 
 })//$document.ready
